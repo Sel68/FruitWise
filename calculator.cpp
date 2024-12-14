6b0d2e6b4955e4d4120b8d8d0d625e667e36d972
sel68 <saadhasanellahie@gmail.com>
2024-12-14 21:11:50 +0500

    Added values and userChoice function

--- calculator.cpp
+++ calculator.cpp
@@ -4,31 +4,30 @@ using namespace std;
 #include <fstream>
 #include <ctime>
 
-string fruits[] = {"orange", "apple", "banana"}; //string array of fruits
-const int fruitCount=3;
+string fruits[] = {"orange", "apple", "banana"}; // string array of fruits
+const int fruitCount = 3;
 
+double parameters[3][4] = {{6, 1, 0.05, 0.01}, {8, 1, 0.04, 0.008}, {4, 1, 0.07, 0.015}}; // 2-D array  of parameters for each fruit {orange, apple,
+// banana}
 
-double parameters[3][4]= {{6, 1, 0.05, 0.01}, {8, 1, 0.04, 0.008},
-        {4, 1, 0.07, 0.015}}; //2-D array  of parameters for each fruit {orange, apple, 
-        // banana}
-
-
-double shelfLife(string fruit, double score, double temp, double hum){    
+double shelfLife(string fruit, double score, double temp, double hum)
+{
     int i;
-    for(i=0; i<fruitCount; i++){
-        if(fruit == fruits[i])
+    for (i = 0; i < fruitCount; i++)
+    {
+        if (fruit == fruits[i])
             break;
     }
     double life;
-    life = parameters[i][0] * score / (parameters[i][1] +parameters[i][2] * temp + parameters[i][3] * hum);
+    life = parameters[i][0] * score / (parameters[i][1] + parameters[i][2] * temp + parameters[i][3] * hum);
     return life;
 }
 
 string currentTime()
 {
-    time_t curr = time(0); //current time
+    time_t curr = time(0); // current time
     char buffer[80];
-    struct tm timeComp; //structure to hold time broken into components
+    struct tm timeComp; // structure to hold time broken into components
     timeComp = *localtime(&curr);
     strftime(buffer, sizeof(buffer), "%Y-%m-%d %X", &timeComp);
     return string(buffer);
@@ -39,7 +38,7 @@ string calculateReminder(double shelfLife)
     time_t current = time(0);
     struct tm reminder = *localtime(&current);
 
-    int additionalHours = int((shelfLife * 24)) - 1; //reminder set for one hour before
+    int additionalHours = int((shelfLife * 24)) - 1; // reminder set for one hour before
 
     reminder.tm_hour += additionalHours;
 
@@ -51,53 +50,64 @@ string calculateReminder(double shelfLife)
     return string(buffer);
 }
 
+void userChoice(double &temp, double &hum)
+{
+    cout << "Please press Y to enter custom temperature and humidity values, or N to use preset Topi values.\n";
+    char c;
+    cin >> c;
+    while (c != 'Y' && c != 'N')
+    {
+        cout << "Incorrect keypress.\n";
+        cout << "Please press Y to enter custom temperature and humidity values, or N to use preset Topi December averages.\n";
+        cin >> c;
+    }
 
+    if (c == 'Y')
+    {
+        cout << "Please enter temperature: ";
+        cin >> temp;
+        cout << "Please enter humidity percentage: \n";
+        cin >> hum;
+    }
+}
 
-int main(){
+void values(double &score, double &temp, double &hum)
+{
+    cout << "Please enter the health score of your fruit. (0-5, floating point allowed)\n";
+    cin >> score;
+
+    temp = 22.0; // Dec high, low = 19,0, 8.1 (Topi)
+    hum = 46.0;  // Dec Avg (Topi)
+
+    userChoice(temp, hum);
+}
+
+int main()
+{
 
     ifstream fruitText;
-    fruitText.open("E:\\CP\\CS101 Project\\Code\\fruit_output.txt"); 
-    //file saved as fruit_output.txt
+    fruitText.open("E:\\CP\\CS101 Project\\Code\\fruit_output.txt");
+    // file saved as fruit_output.txt
 
-    if(!fruitText)
-        cout<<"Unable to open file " << "fruit_output.txt"<<endl;
-    else{
-        
+    if (!fruitText)
+        cout << "Unable to open file " << "fruit_output.txt" << endl;
+    else
+    {
+
         string fruit;
         getline(fruitText, fruit);
         fruitText.close();
 
-        double score;
-        cout<<"Please enter the health score of your fruit. (0-5, floating point allowed)\n";
-        cin>>score;
-
-        double temp = 22.0; // Dec high, low = 19,0, 8.1 (Topi)
-        double hum = 46.0; // Dec Avg (Topi)
-
-        cout<<"Please press Y to enter custom temperature and humidity values, or N to use preset Topi values.\n";
-        char c;
-        cin>>c;
-        while(c!= 'Y' && c!= 'N'){
-            cout<<"Incorrect keypress.\n";
-            cout<<"Please press Y to enter custom temperature and humidity values, or N to use preset Topi December averages.\n";
-            cin>>c;
-        }
-
-        if(c=='Y'){
-            cout<<"Please enter temperature: ";
-            cin>>temp;
-            cout<<"Please enter humidity percentage: \n";
-            cin>> hum;
-        }
-
-
+        double score, temp, hum;
+        values(score, temp, hum);
 
         double life = shelfLife(fruit, score, temp, hum);
         string reminder = calculateReminder(life);
 
         ofstream shelfLifeOutput("shelf_life.txt", ios::app);
 
-        if(shelfLifeOutput.is_open()){
+        if (shelfLifeOutput.is_open())
+        {
             shelfLifeOutput << "Date: " << currentTime() << "\n";
             shelfLifeOutput << "Fruit: " << fruit << "\n";
             shelfLifeOutput << "Health Score: " << score << "\n";
@@ -106,11 +116,9 @@ int main(){
             shelfLifeOutput.close();
             cout << "Shelf life: " << life << "\n";
             cout << "Reminder set for " << reminder << " to finish your " << fruit << "s :)" << endl;
-
         }
         else
-            cout<<"Unable to open shelf_life.txt"<<endl;
-
+            cout << "Unable to open shelf_life.txt" << endl;
     }
 
     return 0;
